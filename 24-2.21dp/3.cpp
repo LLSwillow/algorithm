c61ef2d8780e07f24fd4b0d7a4e97d18c861c3f2
LLSwillow <18237916376@163.com>
2024-02-29 21:15:50 +0800

    完全背包

---
+++ 24-2.21dp/3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+const int N=1010;
+int n,m;
+int v[N],w[N];
+int dp[N][N];
+int main()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)cin>>v[i]>>w[i];
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            for(int k=0;k*v[i]<=j;k++)
+            {
+                dp[i][j]=max(dp[i][j],dp[i-1][j-k*v[i]]+k*w[i]);
+            }
+        }
+    }
+    cout<<dp[n][m];
+    return 0;
+}
